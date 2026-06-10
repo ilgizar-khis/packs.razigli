34814d81c684a8d682b83e6fda5f21e2e4ef1f48
razigli <razigli@arch.dev>
2026-06-10 23:34:52 +0500

    fix error

--- lua/packs/razigli.lua
+++ lua/packs/razigli.lua
@@ -75,12 +75,11 @@ end
 
 -- toggle status of pkg
 function M.toggle_status()
-	local lineNr = vim.api.nvim_win_get_cursor(M.win)[1]
 	local buf = vim.api.nvim_win_get_buf(M.win)
 	if buf == M.clear_buf then
 		local number = M.get_prev_line(true)
-		local line = vim.api.nvim_buf_get_lines(M.clear_buf, number, number, false)
-		vim.notify(line)
+		local line = vim.api.nvim_buf_get_lines(M.clear_buf, number - 1, number, false)[1]
+		vim.notify(line .. " sdaf")
 	end
 end
 
