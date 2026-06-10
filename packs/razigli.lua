ae23beec494164e9563bd09773853757d43e5316
razigli <razigli@arch.dev>
2026-06-10 18:03:45 +0500

    add setup function

--- packs/razigli.lua
+++ packs/razigli.lua
@@ -1,6 +1,8 @@
 local M = {}
 
-vim.api.nvim_create_user_command("Packs", function()
-	vim.notify("TEST")
-end, {})
+function M.setup()
+	vim.api.nvim_create_user_command("Packs", function()
+		vim.notify("TEST")
+	end, {})
+end
 return M
