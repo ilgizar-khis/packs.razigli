d0872dc4c0dd3e29e7a8ac40d13fd5e567c0b41a
razigli <razigli@arch.dev>
2026-06-10 18:38:29 +0500

    rename \"open\" to \"toggle\" and add win

--- lua/packs/razigli.lua
+++ lua/packs/razigli.lua
@@ -4,12 +4,30 @@ local M = {
 	clear_buf = nil,
 	list_buf = nil,
 	update_buf = nil,
-	params = {},
+	params = {
+		width = 160,
+		height = 40,
+		border = "single",
+	},
 }
 
-function M.open()
+function M.toggle()
 	if not M.main_buf then
 		M.main_buf = vim.api.nvim_create_buf(false, true)
+	end
+
+	local col = math.floor((vim.api.nvim_get_option("columns") - M.width) / 2)
+	local row = math.floor((vim.api.nvim_get_option("lines") - M.height) / 2)
+
+	if not M.win or not vim.api.nvim_win_is_valid(M.win) then
+		M.win = vim.api.nvim_open_win(M.main_buf, true, {
+			relative = "editor",
+			width = M.width,
+			height = M.height,
+			col = col,
+			row = row,
+			border = M.border,
+		})
 	end
 end
 
