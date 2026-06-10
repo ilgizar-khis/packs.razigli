3f057637a0a81fca9a83295d30016f10119bb93e
razigli <razigli@arch.dev>
2026-06-10 23:17:00 +0500

    create get_prev_line function

--- lua/packs/razigli.lua
+++ lua/packs/razigli.lua
@@ -35,9 +35,8 @@ function M.to_next()
 	vim.api.nvim_win_set_cursor(M.win, { 1, 3 })
 end
 
--- goto prev pkg
-function M.to_prev()
-	-- get datas
+-- get prev pkg_name line number
+function M.get_prev_line()
 	local lineNr = vim.api.nvim_win_get_cursor(M.win)[1]
 	local buf = vim.api.nvim_win_get_buf(M.win)
 	local lines = vim.api.nvim_buf_get_lines(buf, 0, lineNr, false)
@@ -48,10 +47,17 @@ function M.to_prev()
 		if string.find(line, "^%[[%+%- ]%]") then
 			-- jump to next name
 			vim.api.nvim_win_set_cursor(M.win, { i, 3 })
-			print("line: " .. i)
 			return
 		end
 	end
+end
+
+-- goto prev pkg
+function M.to_prev()
+	-- get datas
+	local lineNr = vim.api.nvim_win_get_cursor(M.win)[1]
+	local buf = vim.api.nvim_win_get_buf(M.win)
+	local lines = vim.api.nvim_buf_get_lines(buf, 0, lineNr, false)
 	-- jump to start
 	local last_line = vim.api.nvim_buf_line_count(buf)
 	vim.api.nvim_win_set_cursor(M.win, { last_line, 3 })
