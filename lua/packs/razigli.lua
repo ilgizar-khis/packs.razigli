c02faf02bf89257225505a7d0a67382fd33a7c9a
razigli <razigli@arch.dev>
2026-06-10 21:38:43 +0500

    rename keymap params

--- lua/packs/razigli.lua
+++ lua/packs/razigli.lua
@@ -7,12 +7,10 @@ local M = {
 	width = 160,
 	height = 40,
 	border = "single",
-	keymap = {
-		home_buf = "1",
-		clear_buf = "2",
-		clear = "<CR>",
-		update = "<S-u>",
-	},
+	key_home_buf = "1",
+	key_clear_buf = "2",
+	key_clear = "<CR>",
+	key_update = "<S-u>",
 }
 
 function M.clear()
@@ -70,11 +68,11 @@ function M.toggle()
 		-- add bufferline to home_buf
 		vim.api.nvim_buf_set_lines(M.home_buf, 0, 0, true, { "[1:home]  2:clear", "" })
 		-- keymap to jump clear_buf
-		vim.keymap.set("n", M.keymap.clear_buf, function()
+		vim.keymap.set("n", M.key_clear_buf, function()
 			vim.api.nvim_win_set_buf(M.win, M.clear_buf)
 		end, { buffer = M.home_buf })
 		-- update data
-		vim.keymap.set("n", M.keymap.update, function()
+		vim.keymap.set("n", M.key_update, function()
 			M.update()
 		end, { buffer = M.home_buf })
 	end
@@ -84,15 +82,15 @@ function M.toggle()
 		-- add bufferline to home_buf
 		vim.api.nvim_buf_set_lines(M.clear_buf, 0, 0, true, { " 1:home  [2:clear]", "" })
 		-- keymap to jump home_buf
-		vim.keymap.set("n", M.keymap.home_buf, function()
+		vim.keymap.set("n", M.key_home_buf, function()
 			vim.api.nvim_win_set_buf(M.win, M.home_buf)
 		end, { buffer = M.clear_buf })
 		-- keymap to clear
-		vim.keymap.set("n", M.keymap.clear, function()
+		vim.keymap.set("n", M.key_clear, function()
 			M.clear()
 		end, { buffer = M.clear_buf })
 		-- keymap to update
-		vim.keymap.set("n", M.keymap.update, function()
+		vim.keymap.set("n", M.key_update, function()
 			M.update()
 		end, { buffer = M.clear_buf })
 	end
