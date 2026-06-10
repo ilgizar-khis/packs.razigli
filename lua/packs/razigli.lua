4fc6cd0dac03a8ae7a1f51906e614152c8464c66
razigli <razigli@arch.dev>
2026-06-10 23:01:44 +0500

    use to_prev in home_buf_setup

--- lua/packs/razigli.lua
+++ lua/packs/razigli.lua
@@ -142,6 +142,9 @@ function M.home_buf_setup()
 		vim.keymap.set("n", M.key_to_next, function()
 			M.to_next()
 		end, { buffer = M.home_buf })
+		vim.keymap.set("n", M.key_to_prev, function()
+			M.to_prev()
+		end, { buffer = M.home_buf })
 	end
 end
 
