bde8d7aa2e151066a65f023d4f2ee2c329600720
razigli <razigli@arch.dev>
2026-06-10 22:41:30 +0500

    fix typo

--- lua/packs/razigli.lua
+++ lua/packs/razigli.lua
@@ -84,7 +84,7 @@ end
 
 -- home_buf setup function
 function M.home_buf_setup()
-	if not M.home_buf or not vim.api.nvim_buf_is_valid(M.clear_buf) then
+	if not M.home_buf or not vim.api.nvim_buf_is_valid(M.home_buf) then
 		M.home_buf = vim.api.nvim_create_buf(false, true)
 		-- keymap to jump clear_buf
 		vim.keymap.set("n", M.key_clear_buf, function()
